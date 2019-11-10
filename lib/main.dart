447bd8eae17954f7eb2bc9876cf704877e554645
HUSAM NASRALLAH <husamnasrallah@HUSAMs-iMac-Pro.local>
2019-11-10 13:23:23 +0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    home: HomePage(),
+  )
+);
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Title'),
+      ),
+      body: Container(),
+    );
+  }
+}
