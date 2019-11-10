f0bbad1e58aad998917fe7b453615c864dcab9de
HUSAM NASRALLAH <husamnasrallah@HUSAMs-iMac-Pro.local>
2019-11-10 13:41:09 +0300

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-  MaterialApp(
-    home: HomePage(),
-  )
-);
+void main() => runApp(MaterialApp(
+      home: HomePage(),
+    ));
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -12,10 +10,42 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Zomato'),
-      ),
-      body: Container(),
-    );
+        appBar: AppBar(),
+        body: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('DELICIOUS TURKISH FOOD',
+                      style: TextStyle(color: Colors.grey[500])),
+                  Row(
+                    children: <Widget>[
+                      Text('See all', style: TextStyle(color: Colors.red)),
+                      Icon(Icons.arrow_right, color: Colors.red),
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 200,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Container(
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: Color(0xfff35437),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Column(),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
